735dc05e205c181b5e67d612862634b44abfea7e
abarr0 <alec.barr.uno@gmail.com>
2014-02-03 08:32:52 -0600

    Adding Examples
    Added all examples

---
+++ Exercise Files/Chapter 10/10_02/sandbox/second_page.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+	<head>
+		<title>Second Page</title>
+	</head>
+	<body>
+
+			<?php
+				print_r($_GET)
+			?>
+		
+	</body>
+</html>
